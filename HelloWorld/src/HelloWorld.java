1181a233f77532ce651275b5a96a6686bd09e718
idrisahmed251 <idrisahmed@outlook.com>
2017-08-01 22:54:18 +0100

    fixed a bug. now each class has a tree. before only a file would get a tree.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -14,14 +14,30 @@ public class HelloWorld {
 			 if (file.isFile()){
 				 String filePath = file.getAbsolutePath();
 				 trees.add(new TreeNode<Activity>(new Activity(StatementType.NEW_TREE, filePath, 1, null)));
-				 parse(readFileToString(filePath), filePath, trees.get(trees.size() - 1));	 
+				 parse(readFileToString(filePath), filePath); //, trees.get(trees.size() - 1)	 
 			 }
 
 		 //for (TreeNode<Activity> tree : trees)	printTree(tree); //PRINT THE TREE FROM ROOT NODE GOING LEFT TO RIGHT(PARENT BEFORE CHILD)
 		/* for (Activity a : methodsCalled.keySet())
 			 System.out.println(a.statementType + "\t" + a.astNode + "\n" + methodsCalled.get(a));*/
 		 //THIS WILL PRINT OUT ALL THE METHODS. SOME METHODS MAY NOT HAVE ANY VALUES IN THE HASHMAP SO YOU WILL NEED TO CHECK FOR NULL IN FUTURE.
+		 /*for (TreeNode<Activity> tna : trees)
+			 if (tna.astNodePayload.astNode == null)
+				 trees.remove(tna);
+		 System.out.println(trees.size());*/
 		 
+		 for (Iterator<TreeNode<Activity>> i = trees.iterator(); i.hasNext(); ) {
+		        if (i.next().astNodePayload.astNode == null) {
+		            i.remove();
+		            
+		        } 
+		    }
+		 for (TreeNode<Activity> tna : trees){
+			 System.out.println(tna.children.size());
+			 System.out.println("-----------------------------");
+		 }
+		// System.out.println(trees.size());
+
 	}
 
 	public static String readFileToString(String filePath) throws IOException {
@@ -38,7 +54,7 @@ public class HelloWorld {
 		return  fileData.toString();	
 	}
 
-	public static void parse(String file, String path, TreeNode<Activity> tree) {
+	public static void parse(String file, String path) {
 		ASTParser parser = ASTParser.newParser(AST.JLS3);
 		parser.setSource(file.toCharArray());
 		parser.setProject(null);
@@ -81,6 +97,7 @@ public class HelloWorld {
 			*/
 			
 			public boolean visit(MethodInvocation astNode) {
+				TreeNode<Activity> tree = trees.get(trees.size() - 1);
 				MethodDeclaration parentNode = (MethodDeclaration) getDeclaredMethod(astNode);
 				Activity activity = new Activity(StatementType.METHOD_INVOCATION, path, cu.getLineNumber(astNode.getStartPosition()), astNode);
 				TreeNode<Activity> nodeInTree = findMatchingNodeInTree(parentNode, tree);
@@ -92,6 +109,7 @@ public class HelloWorld {
 			}
 
 			public boolean visit(VariableDeclarationFragment node) {
+				TreeNode<Activity> tree = trees.get(trees.size() - 1);
 				ASTNode parentNode = getDeclaredMethod(node);
 				if (parentNode.getNodeType() == 31)
 					parentNode = (MethodDeclaration) getDeclaredMethod(node);
@@ -131,10 +149,41 @@ public class HelloWorld {
 			}
 
 			public boolean visit(MethodDeclaration astNode) {
+				TreeNode<Activity> tree = trees.get(trees.size() - 1);
 				MethodDeclarationActivity activity = new MethodDeclarationActivity(StatementType.METHOD_DECLARATION, path, cu.getLineNumber(astNode.getStartPosition()), astNode);
+				
+				ASTNode temp = astNode;
+				while(temp.getNodeType() != 15) {
+					//System.out.println(temp.getNodeType());
+					if (temp.getNodeType() == 55) {
+						if (temp != tree.astNodePayload.astNode) {
+							//System.out.println("---------------------" + tree.astNodePayload.astNode);
+							TreeNode<Activity> newNode = new TreeNode<Activity>(new Activity(StatementType.NEW_TREE, path, cu.getLineNumber(temp.getStartPosition()), temp));
+							trees.add(newNode);
+							tree = trees.get(trees.size() - 1);
+							//System.out.println("found one");
+						}
+					}
+					temp = temp.getParent();
+				
+				}
+				//System.out.println("_______________________");
+				
+				
+				
+				
 				tree.addChild(activity);
 				methodsCalled.put(activity, new HashSet<Activity>());
-				return true;
+				/*ASTNode temp = astNode;
+				//System.out.println("temp node type:" + temp.getNodeType());
+				while (temp.getParent() != null){
+					if (temp.getNodeType() == 55) 
+						System.out.println(temp);
+					System.out.println(temp.getNodeType());
+					temp = temp.getParent();
+				}
+				System.out.println("_________________");*/
+					return true;
 			}
 		});
 	}
