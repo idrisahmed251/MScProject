9753c4ee296a1125769ae9f4005354bc72a4072a
idrisahmed251 <idrisahmed@outlook.com>
2017-08-03 16:14:29 +0100

    Massive amounts of refactoring.

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,11 +1,16 @@
 import java.io.*;
 import java.util.*;
+import java.util.Map.Entry;
+
 import org.eclipse.jdt.core.dom.*;
+
+enum StatementType { METHOD_DECLARATION, CONDITIONAL_STATEMENT, REPEATED_EXECUTION, NEW_TREE, VARIABLE_DECLARATION, METHOD_INVOCATION }
+
 
 public class HelloWorld {
 	
 	static ArrayList<TreeNode<Activity>> trees = new ArrayList<TreeNode<Activity>>();
-	static Map<Activity, Set<Activity>> methodsCalled = new HashMap<Activity, Set<Activity>>();
+	static Map<Activity, Set<Activity>> methodsInvokedInMethods = new HashMap<Activity, Set<Activity>>();
 	
 	public static void main(String[] args) throws IOException {
 		File root = new File(new File(".").getCanonicalPath() + File.separator+"src"+File.separator);
@@ -14,26 +19,18 @@ public class HelloWorld {
 			 if (file.isFile()){
 				 String filePath = file.getAbsolutePath();
 				 trees.add(new TreeNode<Activity>(new Activity(StatementType.NEW_TREE, filePath, 1, null)));
-				 parse(readFileToString(filePath), filePath); //, trees.get(trees.size() - 1)	 
+				 parse(readFileToString(filePath), filePath);	 
 			 }
-
-		 //for (TreeNode<Activity> tree : trees)	printTree(tree); //PRINT THE TREE FROM ROOT NODE GOING LEFT TO RIGHT(PARENT BEFORE CHILD)
-		/* for (Activity a : methodsCalled.keySet())
-			 System.out.println(a.statementType + "\t" + a.astNode + "\n" + methodsCalled.get(a));*/
-		 //THIS WILL PRINT OUT ALL THE METHODS. SOME METHODS MAY NOT HAVE ANY VALUES IN THE HASHMAP SO YOU WILL NEED TO CHECK FOR NULL IN FUTURE.
-
-		 removeNullNodesFromTrees();		
-		 for (TreeNode<Activity> tree : trees)	printTree(tree);
-		 //System.out.println(trees.get(0).getLastNode().getActivity());
+		 cleanDataUp();
 	}
-
-	public static String readFileToString(String filePath) throws IOException {
+	
+	private static String readFileToString(String filePath) throws IOException {
 		StringBuilder fileData = new StringBuilder(1000);
 		BufferedReader reader = new BufferedReader(new FileReader(filePath));
 
 		char[] buf = new char[10];
 		int numRead = 0;
-		while ((numRead = reader.read(buf)) != -1) { //-1 == eof
+		while ((numRead = reader.read(buf)) != -1) {
 			fileData.append(new String(String.valueOf(buf, 0, numRead)));
 			buf = new char[1024];
 		}
@@ -41,90 +38,77 @@ public class HelloWorld {
 		return  fileData.toString();	
 	}
 
-	public static void parse(String file, String path) {
+	private static void parse(String file, String path) {
 		ASTParser parser = ASTParser.newParser(AST.JLS3);
 		parser.setSource(file.toCharArray());		
 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
-		final CompilationUnit cu = (CompilationUnit) parser.createAST(null);
-
+		final CompilationUnit cu = (CompilationUnit) parser.createAST(null);		
+		
 		cu.accept(new ASTVisitor() {
 			
-/*			public boolean visit(MethodInvocation astNode) {
-				TreeNode<Activity> tree = trees.get(trees.size() - 1);
-				Activity activity = new Activity(StatementType.METHOD_INVOCATION, path, cu.getLineNumber(astNode.getStartPosition()), astNode);
-				
-				MethodDeclaration parentNode = (MethodDeclaration) getDeclaredMethod(astNode);
-				TreeNode<Activity> matchedTreeNode = findMatchingNodeInTree(parentNode, tree);
-				if (matchedTreeNode == null) tree.addChild(activity); 
-				else matchedTreeNode.addChild(activity);
-				
-				methodsCalled.get(matchedTreeNode.getActivity()).add(activity);
-				return true;
-			}*/
-
-			public boolean visit(VariableDeclarationFragment node) {
-				ASTNode whereTheVariableIsDefined = getDeclaredMethod(node);
-				TreeNode<Activity> tree = getCorrectTreeForNode(trees.get(trees.size() - 1), whereTheVariableIsDefined);
-				Activity activity = new Activity(StatementType.VARIABLE_DECLARATION, path, cu.getLineNumber(node.getStartPosition()), node);
-				if (whereTheVariableIsDefined.getNodeType() == 31)
-					findMatchingNodeInTree(whereTheVariableIsDefined, tree).addChild(activity);
-				else
-					tree.addChild(activity);
-				return false; // do not continue to avoid usage info
-			}
-
-			public TreeNode<Activity> findMatchingNodeInTree(ASTNode parentNode, TreeNode<Activity> tree) {
-				Activity activityInTreeNode = tree.getActivity();
-				TreeNode<Activity> oneToReturn = tree;
-				
-				if (activityInTreeNode.getPayLoad() == null || activityInTreeNode.getPayLoad() != parentNode) {
-					for (TreeNode<Activity> tna : tree.getAllChildren())
-						if (tna.getActivity().getPayLoad() == parentNode) {
-							oneToReturn = tna;
-							break;
-						}
-				}
-				
-				return oneToReturn;
-			}
-			
-			
-
-			public ASTNode getDeclaredMethod(ASTNode node) {
-				ASTNode tempNode = node;
-				while (tempNode.getNodeType() != 15) { //15 = file
-					if (tempNode.getNodeType() == 31) { //31 = method
-						System.out.println(tempNode);
-						return tempNode;
-					}else if (tempNode.getNodeType() == 55) {//55=class
-						return tempNode;
-					}
-					tempNode = tempNode.getParent();
-				}
-				if(tempNode.getNodeType() == 15) System.out.println(tempNode);
-				System.out.println("==========================");
-				return tempNode;
-			}
-
 			public boolean visit(MethodDeclaration astNode) {
-				TreeNode<Activity> tree = getCorrectTreeForNode(trees.get(trees.size() - 1), astNode);;
-				MethodDeclarationActivity activity = new MethodDeclarationActivity(StatementType.METHOD_DECLARATION, path, cu.getLineNumber(astNode.getStartPosition()), astNode);
+				TreeNode<Activity> tree = getRightTreeForASTNode(getLastDefinedTree(), astNode);
+				MethodDeclarationActivity activity = new MethodDeclarationActivity(StatementType.METHOD_DECLARATION, path, getLineNumber(astNode), astNode);
 				tree.addChild(activity);
-				methodsCalled.put(activity, new HashSet<Activity>());
+				methodsInvokedInMethods.put(activity, new HashSet<Activity>());
+				return true;
+			}
+			
+			public boolean visit(VariableDeclarationFragment astNode) {
+				ASTNode whereTheVariableIsDefined = getMethodOrClassNodeBelongsTo(astNode);
+				TreeNode<Activity> tree = getRightTreeForASTNode(getLastDefinedTree(), whereTheVariableIsDefined);
+				Activity activity = new Activity(StatementType.VARIABLE_DECLARATION, path, getLineNumber(astNode), astNode);
+				
+				findMatchingMethodInTree(whereTheVariableIsDefined, tree).addChild(activity);
+				return false;
+			}
+			
+			public boolean visit(MethodInvocation astNode) {
+				TreeNode<Activity> tree = getRightTreeForASTNode(getLastDefinedTree(), astNode);
+				Activity activity = new Activity(StatementType.METHOD_INVOCATION, path, getLineNumber(astNode), astNode);
+				MethodDeclaration methodWhichTheInvocationOccurredIn = (MethodDeclaration) getMethodOrClassNodeBelongsTo(astNode);
+				
+				TreeNode<Activity> matchedTreeNode = findMatchingMethodInTree(methodWhichTheInvocationOccurredIn, tree);
+				matchedTreeNode.addChild(activity);
+				
+				methodsInvokedInMethods.get(matchedTreeNode.getActivity()).add(activity);
 				return true;
 			}
 
-			private TreeNode<Activity> getCorrectTreeForNode(TreeNode<Activity> thisTree, ASTNode astNode) {
-				while (astNode.getNodeType() != 15) {//15 = file
-					if (astNode.getNodeType() == 55) //55 = class
-						if (astNode != thisTree.getActivity().getPayLoad()) {
-							TreeNode<Activity> newTree = new TreeNode<Activity>(new Activity(StatementType.NEW_TREE, path, cu.getLineNumber(astNode.getStartPosition()), astNode));
+			private TreeNode<Activity> getRightTreeForASTNode(TreeNode<Activity> lastDefinedTree, ASTNode astNode) {
+				while (astNode.getNodeType() != 15) {
+					if (astNode.getNodeType() == 55)
+						if (astNode != lastDefinedTree.getActivity().getASTNode()) {
+							TreeNode<Activity> newTree = new TreeNode<Activity>(new Activity(StatementType.NEW_TREE, path, getLineNumber(astNode), astNode));
 							trees.add(newTree);
 						}
 					astNode = astNode.getParent();
 				}
-				return trees.get(trees.size() - 1);
+				return getLastDefinedTree();
 			}
+			
+			private TreeNode<Activity> getLastDefinedTree() { return trees.get(trees.size() - 1); }
+			
+			private int getLineNumber(ASTNode astNode) { return cu.getLineNumber(astNode.getStartPosition()); }
+			
+			private ASTNode getMethodOrClassNodeBelongsTo(ASTNode node) {
+				while (node.getNodeType() != 15) {
+					if (node.getNodeType() == 31) return node; else if (node.getNodeType() == 55) return node;
+					node = node.getParent();
+				}
+				return null;
+			}
+
+			//TODO: currently only works for class methods and class variables. In future, would like it to find any node in any tree.
+			private TreeNode<Activity> findMatchingMethodInTree(ASTNode nodeToFind, TreeNode<Activity> tree) {
+				ASTNode astInTreeNode = tree.getActivity().getASTNode();
+				TreeNode<Activity> matchedNode = tree;
+				
+				if (astInTreeNode != nodeToFind)
+					for (TreeNode<Activity> child : tree.getAllChildren())
+						if (child.getActivity().getASTNode() == nodeToFind) matchedNode = child;
+				return matchedNode;
+			}			
 		});
 	}
 	
@@ -132,34 +116,18 @@ public class HelloWorld {
 		tree.print();
 		if (tree.hasChild()) for (TreeNode<Activity> child : tree.getAllChildren()) printTree(child);
 	}
-	
-	
-	private static void removeNullNodesFromTrees() {
+
+	private static void cleanDataUp() {
 		 for (Iterator<TreeNode<Activity>> i = trees.iterator(); i.hasNext(); ) 
-		        if (i.next().getActivity().getPayLoad() == null) 
+		        if (i.next().getActivity().getASTNode() == null) 
 		            i.remove();
+		 for(Iterator<Entry<Activity, Set<Activity>>> j = methodsInvokedInMethods.entrySet().iterator(); j.hasNext(); ) {
+			 Entry<Activity, Set<Activity>> entry = j.next();
+			 if (entry.getValue().size() == 0)
+				 j.remove();
+		    }
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-enum StatementType { METHOD_DECLARATION, CONDITIONAL_STATEMENT, REPEATED_EXECUTION, NEW_TREE, VARIABLE_DECLARATION, METHOD_INVOCATION }
 
 
 class Activity {
@@ -174,7 +142,7 @@ class Activity {
 	
 	public String getStatementType() { return statementType.toString(); }
 	
-	public ASTNode getPayLoad() { return this.astNode; }
+	public ASTNode getASTNode() { return this.astNode; }
 	
  	public String toString() {
  		return "In file: " + filePath + "\nOn line: " + startLine + " there is a " + statementType + ". The ast node is:\n" + astNode.toString();
@@ -184,8 +152,9 @@ class Activity {
 
 class MethodDeclarationActivity extends Activity {
 
+	//TODO: unsure as to whether or not modifiers is needed. TODO review modifiers and see if variable can be deleted once plugin is created.
 	List modifiers; Type returnType; String methodName; String params; String methodBody;
-	//Unsure as to whether or not modifiers is needed. TODO review modifiers and see if variable can be deleted once plugin is created.
+	
 	public MethodDeclarationActivity(StatementType statementType, String filePath, int startLine, ASTNode node) {
 		super(statementType, filePath, startLine, node);
 		MethodDeclaration n = (MethodDeclaration) node;
@@ -194,6 +163,7 @@ class MethodDeclarationActivity extends Activity {
 }
 
 
+//TODO: toString throws StackOverflow area ergo, print was created. find out why and use toString i.e. the standard.
 class TreeNode<Activity> {
 	private TreeNode<Activity> parentNode = null; 
 	private Activity astNodePayload; 
@@ -215,16 +185,7 @@ class TreeNode<Activity> {
 	
 	public Boolean hasChild() { if (children.size() > 0) return true; else return false; }
 	
-	public TreeNode<Activity> getChild(int index) { return children.get(index); }
-	
 	public List<TreeNode<Activity>> getAllChildren() { return children;	}
-	
- 	public TreeNode<Activity> getLastNode() {
-		TreeNode<Activity> lastTreeNode = this;
-		while (lastTreeNode.hasChild()) 
-			if (lastTreeNode.hasChild()) lastTreeNode = children.get(children.size() - 1);
-		return lastTreeNode;
-	}	
 	
 	public void print() {
 		if (this.isRootNode())
@@ -237,3 +198,9 @@ class TreeNode<Activity> {
 //http://www.programcreek.com/2011/11/use-jdt-astparser-to-parse-java-file/
 //https://stackoverflow.com/questions/3522454/java-tree-data-structure
 //http://www.programcreek.com/2011/07/find-all-callers-of-a-method/ GET ALL CALLERS OF A METHOD
+/*
+ *AST NODE TYPES:
+ *15 = file
+ *55 = class
+ *31 = method
+*/
